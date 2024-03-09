65ccda67813c83398ace0bca55d277ebffbd4272
ChrisJacobs2 <cmjacobs@uncg.edu>
2024-03-08 21:56:46 -0500

    Weapon spawner created. Now I just need to implement character holdign weapons

--- Godot_Project/GameLogic/Weapons/Weapon_Spawner.gd
+++ Godot_Project/GameLogic/Weapons/Weapon_Spawner.gd
@@ -1,32 +1,67 @@
 extends Node2D
 
+# Does the spawner need to cool down?
+var cooldown = false
+
+# How many players are inside of the spawner?
+var num_players_inside = 0
+
+# 5 second cooldown
+var cooldown_timer = 5.0
+
+var chosen_gun = preload("res://GameLogic/Weapons/PenguinGun.tscn").instantiate()
+
 func spawn():
-	print("spawn")
+	print("weapon spawned")
 	# Add an instance of the PenguinGun.tscn (located in res://GameLogic/Weapons/PenguinGun.tscn)
-	 #as a child of the node called Weapon
+	#as a child of the node called Weapon
 	# This can be randomized later
-	var penguin_gun = preload("res://GameLogic/Weapons/PenguinGun.tscn").instantiate()
-	$Weapon.add_child(penguin_gun)
-	pass
-	
+	$Weapon.add_child(chosen_gun)
+
+
+func collect():
+	# Stats
+	print($Weapon.get_child_count())
+	# Remove the gun from the spawner
+	if $Weapon.get_child_count() > 0:
+		$Weapon.remove_child(chosen_gun)
+	else:
+		print("No children to remove")
+
+	# wait cooldown_timer seconds
+	await get_tree().create_timer(cooldown_timer).timeout
+
+	# await until num_players_inside == 0
+	while num_players_inside > 0:
+		await get_tree().create_timer(1.0).timeout
+	# respawn the gun
+	spawn()
+	cooldown = false
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Hide the ColorRect
 	$ColorRect.hide()
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
 
-
-
-
-
+# Called whenever a "body" including the player enters the collision box of the
+# spawner.
 func _on_spawner_area_entered(body):
+	# We keep track of this incase a player stays inside of a spawner
+	num_players_inside += 1
 	print("Spawner Area Entered")
 	print(body)
-	pass # Replace with function body.
+	
+	# makes sure that the spawner only activates every cooldown_timer seconds
+	if cooldown == false:
+		cooldown = true
+		collect()	
+
+
+func _on_spawner_area_exited(_body):
+	num_players_inside -= 1
