f1478be4da1fbb4a6d7536533c11c477e42dc17a
Christopher Jacobs <cmjacobs@uncg.edu>
2024-03-11 21:33:32 -0400

    Improved comments

--- Godot_Project/GameLogic/SpawnerLogic.gd
+++ Godot_Project/GameLogic/SpawnerLogic.gd
@@ -3,11 +3,12 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# Get all spawners in the current scene, then iterate on them.
 	var spawners = get_tree().get_nodes_in_group("spawner")
 	for spawner in spawners:
 		print(spawner)
 		spawner.spawn()
-	pass # Replace with function body.
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
