4880ae88823eefc5d59e8bb555aa6c4cc3e623e6
Christopher Jacobs <cmjacobs@uncg.edu>
2024-03-19 15:19:03 -0400

    added some assets, worked on socring scene

---
+++ Godot_Project/GameLogic/scoring/score_manager.gd
@@ -0,0 +1,28 @@
+extends Node
+
+var player_scores = []
+
+var players = []
+
+var num_players = 0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func initialize_scores():
+	# Iterate over every player that is currently in the game and add them to the players array
+
+	# For every player in the players array, add a score (0) to the player_scores array
+	pass
+
+func update_score(_player_index, _score):
+	pass
+
+func remove_player(_player_index):
+	# set the score to null
+
+	# decrement num_players by 1, to a minimum of 1
+	pass
