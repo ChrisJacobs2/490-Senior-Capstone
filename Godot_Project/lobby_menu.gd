21072e0ad75d4f05458f550d4f2c59fb2b2fc611
ChrisJacobs2 <cmjacobs@uncg.edu>
2024-03-24 19:30:52 -0400

    added fields for nickname, ip address

--- Godot_Project/lobby_menu.gd
+++ Godot_Project/lobby_menu.gd
@@ -1,12 +1,9 @@
 extends Control
-
+# Implemented by Cynthia Espinoza
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$HBoxContainer/PublicButton.grab_focus()
-
-
-
 
 func _on_back_button_pressed():
 	get_tree().change_scene_to_file("res://start_menu.tscn")
@@ -19,3 +16,13 @@ func _on_public_button_pressed():
 
 func _on_create_button_pressed():
 	get_tree().change_scene_to_file("res://character_selection.tscn")
+
+
+# Networking code beyond this point
+# Implemented by Christopher Jacobs
+
+var port = 4000
+var maxplayers = 4
+
+var nickname = ""
+var player_list = {}
